add3732a618b92aa3df28cb9137ea024a0065b91
mabbas112 <mabbasctn112@gmail.com>
2022-07-26 13:18:55 +0500

    only see app.js for handling side effects in redux

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector, useDispatch } from 'react-redux';
 import { Fragment, useEffect } from 'react';
+import { uiSliceActions } from './store/ui-slice';
 import Notification from './components/UI/Notification'
-import { sendToCart } from './store/cart-slice'
 
 let isInitial = true;
 
@@ -18,12 +18,37 @@ function App() {
 
   useEffect(
     () => {
+      const sendCartData = async () => {
+        dispatch(uiSliceActions.showNotification({
+        status: 'Pending',
+        title: 'Sending',
+        message: 'Sending cart data'
+      }))
+        const response = await fetch('https://module19-c35c0-default-rtdb.firebaseio.com/cart.json', {
+          method: 'PUT',
+          body: JSON.stringify(cart)
+        })
+        if (!response.ok)
+          throw new Error()
+        dispatch(uiSliceActions.showNotification({
+          status: 'success',
+          title: 'Success',
+          message: 'Sent cart data successfully'
+        }))
+      }
+
       if (isInitial) {
         isInitial = false;
         return
       }
 
-      dispatch(sendToCart(cart));
+      sendCartData().catch(() => {
+        dispatch(uiSliceActions.showNotification({
+          status: 'error',
+          title: 'Error',
+          message: 'Sending cart data failed'
+        }))
+      })
 
     }, [cart, dispatch]
   )
